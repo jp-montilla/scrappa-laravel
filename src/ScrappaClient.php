f73ff067f933d0aed7bd64819786c4791b03fcad
John Paul Montilla <johnpaul.montilla1330@gmail.com>
2025-09-02 23:59:02 +0800

    added google review, and fixed structure

--- src/ScrappaClient.php
+++ src/ScrappaClient.php
@@ -3,6 +3,7 @@
 namespace JohnPaulMontilla\Scrappa;
 
 use InvalidArgumentException;
+use JohnPaulMontilla\Scrappa\Support\ScrappaEndpoint;
 
 class ScrappaClient
 {
@@ -11,6 +12,25 @@ class ScrappaClient
     public function __construct(RestClient $client)
     {
         $this->client = $client;
+    }
+
+    /**
+     * Perform an autocomplete on Google Maps
+     *
+     * @param string $query The search query (required)
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    public function autoCompleteGmaps(?string $query = null): array
+    {
+        if (empty($query)) {
+            throw new InvalidArgumentException('Query parameter is required');
+        }
+
+        // Build query parameters
+        $queryParams = ['query' => $query];
+
+        return $this->client->get(ScrappaEndpoint::MAPS_AUTOCOMPLETE, $queryParams);
     }
 
     /**
@@ -32,7 +52,7 @@ class ScrappaClient
             throw new InvalidArgumentException('The "zoom" parameter is required.');
         }
 
-        // Build query parameters - only query is required
+        // Build query parameters
         $queryParams = ['query' => $query];
 
         // Add optional parameters if provided
@@ -45,27 +65,44 @@ class ScrappaClient
             return in_array($key, ['query', 'zoom'], true) || (!is_null($value) && $value !== '');
         }, ARRAY_FILTER_USE_BOTH);
 
-        return $this->client->get('maps/advance-search', $queryParams);
+        return $this->client->get(ScrappaEndpoint::MAPS_ADVANCE_SEARCH, $queryParams);
     }
 
     /**
-     * Perform an autocomplete on Google Maps
+     * Retrieve Google Reviews for a specific place.
      *
-     * @param string $query The search query (required)
+     * @param string $business_id The search query (required)
+     * @param array $params Additional parameters (search, sort, limit, page)
+     * @param integer $params['sort'] (required)
      * @return array
      * @throws InvalidArgumentException
      */
-    public function autoCompleteGmaps(?string $query = null): array
+    public function googleReviewsGmaps(?string $business_id = null, array $params = []): array
     {
-        if (empty($query)) {
-            throw new InvalidArgumentException('Query parameter is required');
+        if (empty($business_id)) {
+            throw new InvalidArgumentException('The "business_id" parameter is required.');
+        }
+
+        if (empty($params['sort'])) {
+            throw new InvalidArgumentException('The "sort" parameter is required.');
         }
 
         // Build query parameters
-        $queryParams = ['query' => $query];
+        $queryParams = ['business_id' => $business_id];
 
-        return $this->client->get('maps/autocomplete', $queryParams);
+        // Add optional parameters if provided
+        if (!empty($params)) {
+            $queryParams = array_merge($queryParams, $params);
+        }
+
+        // Filter out null/empty values except for query
+        $queryParams = array_filter($queryParams, function ($value, $key) {
+            return in_array($key, ['business_id', 'sort'], true) || (!is_null($value) && $value !== '');
+        }, ARRAY_FILTER_USE_BOTH);
+
+        return $this->client->get(ScrappaEndpoint::MAPS_GOOGLE_REVIEW, $queryParams);
     }
+
 
     /**
      * Generic method to make GET requests with parameters
