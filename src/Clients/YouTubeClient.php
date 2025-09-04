8df21430a3650956dd9a8a22fec6e5462398d8d1
John Paul Montilla <johnpaul.montilla1330@gmail.com>
2025-09-05 00:23:15 +0800

    youtube api

--- src/Clients/YouTubeClient.php
+++ src/Clients/YouTubeClient.php
@@ -4,10 +4,30 @@ namespace JohnPaulMontilla\Scrappa\Clients;
 
 use JohnPaulMontilla\Scrappa\RestClient;
 use JohnPaulMontilla\Scrappa\Support\ScrappaEndpoint;
+use JohnPaulMontilla\Scrappa\Exceptions\ScrappaValidationException;
 
 class YouTubeClient
 {
     public function __construct(
         protected RestClient $client
     ) {}
+
+    /**
+     * Perform a youtube vide info search on Youtube
+     *
+     * @param string $url The search url (required)
+     * @return array
+     * @throws ScrappaValidationException
+     */
+    public function video(?string $url = null): array
+    {
+        if (empty($url)) {
+            throw ScrappaValidationException::missingParameter('url');
+        }
+
+        // Build query parameters
+        $queryParams = ['url' => $url];
+
+        return $this->client->get(ScrappaEndpoint::YOUTUBE, $queryParams);
+    }
 }
