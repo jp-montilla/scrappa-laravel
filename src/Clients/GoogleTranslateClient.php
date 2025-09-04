4e291cd69220275a16278a4df9967cd2968b4515
John Paul Montilla <johnpaul.montilla1330@gmail.com>
2025-09-04 23:49:44 +0800

    google translate client api and example response

--- src/Clients/GoogleTranslateClient.php
+++ src/Clients/GoogleTranslateClient.php
@@ -4,10 +4,35 @@ namespace JohnPaulMontilla\Scrappa\Clients;
 
 use JohnPaulMontilla\Scrappa\RestClient;
 use JohnPaulMontilla\Scrappa\Support\ScrappaEndpoint;
+use JohnPaulMontilla\Scrappa\Exceptions\ScrappaValidationException;
 
 class GoogleTranslateClient
 {
     public function __construct(
         protected RestClient $client
     ) {}
+
+    /**
+     * Perform an google translate on Google
+     *
+     * @param array $params The text, source and target translation (required)
+     * @return array
+     * @throws ScrappaValidationException
+     */
+    public function translate(array $params = []): array
+    {
+        if (empty($params['text'])) {
+            throw ScrappaValidationException::missingParameter('text');
+        }
+
+        if (empty($params['source'])) {
+            throw ScrappaValidationException::missingParameter('source');
+        }
+
+        if (empty($params['target'])) {
+            throw ScrappaValidationException::missingParameter('target');
+        }
+
+        return $this->client->get(ScrappaEndpoint::TRANSLATE, $params);
+    }
 }
